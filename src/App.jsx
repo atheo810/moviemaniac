693b33b4985adf3b99ca07bc860d3d19ff76d372
atheo810 <atheos810@gmail.com>
2023-12-07 21:31:55 +0800

    feat: import MovieList

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import MovieList from "./components/MovieList/MovieList";
 
 export default function App() {
   return (
     <div className="app">
       <Navbar />
-      <main>Main Content</main>
+      <main>
+        <MovieList />
+      </main>
     </div>
   );
 }
