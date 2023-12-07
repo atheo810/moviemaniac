7a0a903d5db996dde96c355d65adc92da8253a30
atheo810 <atheos810@gmail.com>
2023-12-07 16:31:23 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <nav className="navbar">Navbar content</nav>
+      <main>Main Content</main>
+    </div>
+  );
+}
