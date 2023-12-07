517ef3baa19182fbf1e77864e4c748bd36e70b35
atheo810 <atheos810@gmail.com>
2023-12-07 20:36:08 +0800

    feat: Navbar & CSS

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+// import CSS
+import "./Navbar.css";
+
+// Import Images
+import Fire from "../../assets/fire.png";
+import Star from "../../assets/glowing-star.png";
+import Party from "../../assets/partying-face.png";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>MovieManiac</h1>
+      <div className="navbar_links">
+        <a href="">
+          Popular <img src={Fire} alt="fire emoji" className="navbar_emoji" />
+        </a>
+        <a href="">
+          Top Rated <img src={Star} alt="star emoji" className="navbar_emoji" />
+        </a>
+        <a href="">
+          Upcoming{" "}
+          <img src={Party} alt="party emoji" className="navbar_emoji" />
+        </a>
+      </div>
+    </nav>
+  );
+}
