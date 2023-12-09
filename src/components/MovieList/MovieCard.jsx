98e49b6ca7204142e6e48fc01bf826147fef4616
atheo810 <atheos810@gmail.com>
2023-12-09 17:41:11 +0800

    feat: desc, image, and link _blank

--- src/components/MovieList/MovieCard.jsx
+++ src/components/MovieList/MovieCard.jsx
@@ -3,25 +3,29 @@ import "./MovieCard.css";
 
 import Star from "../../assets/star.png";
 
-export default function MovieCard() {
+export default function MovieCard({ movie }) {
   return (
-    <a href="" className="movie_card">
+    <a
+      href={`https://www.themoviedb.org/movie/${movie.id}`}
+      target="_blank"
+      className="movie_card"
+    >
       <img
-        src="https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg"
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         alt=""
         className="movie_poster"
       />
       <div className="movie_details">
-        <h3 className="movie_details_heading">Movie Name</h3>
+        <h3 className="movie_details_heading">{movie.original_title}</h3>
         <div className="aling_center movie_date_rate">
-          <p>10-20-2020</p>
+          <p>{movie.release_date}</p>
           <p>
-            8.0 <img src={Star} alt="rating icon" className="card_emoji" />
+            {movie.rate_average}
+            <img src={Star} alt="rating icon" className="card_emoji" />
           </p>
         </div>
         <p className="movie_description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta et
-          deleniti, impedit deserunt nemo libero.
+          {movie.overview.slice(0, 100) + "..."}
         </p>
       </div>
     </a>
