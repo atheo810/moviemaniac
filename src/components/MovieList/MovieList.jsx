88063a30dde1fab688e5416fcd20f2e38b30b9d6
atheo810 <atheos810@gmail.com>
2023-12-12 11:48:43 +0800

    feat: implement FilterGroup

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,6 +4,7 @@ import "./MovieList.css";
 
 import Fire from "../../assets/fire.png";
 import MovieCard from "./MovieCard";
+import FilterGroup from "./FilterGroup";
 export default function MovieList() {
   const [movies, setMovies] = useState([]);
   const [filterMovies, setFilterMovies] = useState([]);
@@ -40,43 +41,18 @@ export default function MovieList() {
           Popular <img src={Fire} alt="fire emoji" className="navbar_emoji" />
         </h2>
         <div className="align_center movie_list_fs">
-          <ul className="align_center movie_filter">
-            <li
-              className={
-                minRating === 8
-                  ? "movie_filter_item active"
-                  : "movie_filter_item"
-              }
-              onClick={() => handleFilter(8)}
-            >
-              8+ Star
-            </li>
-            <li
-              className={
-                minRating === 7
-                  ? "movie_filter_item active"
-                  : "movie_filter_item"
-              }
-              onClick={() => handleFilter(7)}
-            >
-              7+ Star
-            </li>
-            <li
-              className={
-                minRating === 6
-                  ? "movie_filter_item active"
-                  : "movie_filter_item"
-              }
-              onClick={() => handleFilter(6)}
-            >
-              6+ Star
-            </li>
-          </ul>
+          <FilterGroup
+            minRating={minRating}
+            onRatingClick={handleFilter}
+            ratings={[8, 7, 6]}
+          />
+
           <select name="" id="" className="movie_sorting">
             <option value="">Sort By</option>
             <option value="">Date</option>
             <option value="">Rating</option>
           </select>
+
           <select name="" id="" className="movie_sorting">
             <option value="">Ascending</option>
             <option value="">Descending</option>
