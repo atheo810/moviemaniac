32e9f8235785604c20f382bf5d4f81b8148f6886
atheo810 <atheos810@gmail.com>
2023-12-12 11:48:15 +0800

    feat: FilterGroup jsx

---
+++ src/components/MovieList/FilterGroup.jsx
@@ -0,0 +1,19 @@
+export default function FilterGroup({ minRating, onRatingClick, ratings }) {
+  return (
+    <ul className="align_center movie_filter">
+      {ratings.map((rate) => (
+        <li
+          className={
+            minRating === rate
+              ? "movie_filter_item active"
+              : "movie_filter_item"
+          }
+          key={rate}
+          onClick={() => onRatingClick(rate)}
+        >
+          {rate}+ Star
+        </li>
+      ))}
+    </ul>
+  );
+}
