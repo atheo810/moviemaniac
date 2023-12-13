7c1e104133deea8ac72f18d6a81aa4b10e46ab32
atheo810 <atheos810@gmail.com>
2023-12-13 08:01:19 +0800

    feat: using darkmode

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,11 +6,14 @@ import Fire from "../../assets/fire.png";
 import Star from "../../assets/glowing-star.png";
 import Party from "../../assets/partying-face.png";
 
+import DarkMode from "../DarkMode/DarkMode";
+
 export default function Navbar() {
   return (
     <nav className="navbar">
       <h1>MovieManiac</h1>
       <div className="navbar_links">
+        <DarkMode />
         <a href="">
           Popular <img src={Fire} alt="fire emoji" className="navbar_emoji" />
         </a>
